891e1a404ad7f0d7d3400e0ee7463360f9c960a4
Treiber, Jan <jntreiber@gmail.com>
2023-12-09 17:45:59 +0100

    PR7 fertig

--- PR7/shoppingcart.cpp
+++ PR7/shoppingcart.cpp
@@ -1,7 +1,25 @@
 #include "shoppingcart.h"
 
-ShoppingCart::ShoppingCart(QObject *parent)
-    : QObject{parent}
-{
+QString ShoppingCart::getName() const{
+    return m_name;
+}
 
+QDate ShoppingCart::getDate() const{
+    return m_date;
+}
+
+double ShoppingCart::getTotal() const{
+    return m_total;
+}
+
+void ShoppingCart::setName(QString name){
+    m_name = name;
+}
+
+void ShoppingCart::setDate(QDate date) {
+    m_date = date;
+}
+
+void ShoppingCart::setTotal(double total){
+    m_total = total;
 }
--- PR7/shoppingcart.h
+++ PR7/shoppingcart.h
@@ -2,34 +2,31 @@
 #define SHOPPINGCART_H
 
 #include <QObject>
+#include <QDate>
 
 class ShoppingCart : public QObject
 {
     Q_OBJECT
     Q_CLASSINFO("Description", "A shopping cart of an e-commerce shop")
-    Q_PROPERTY(type m_name READ getName WRITE setName)
-    Q_PROPERTY(type m_date READ getDate WRITE setDate)
-    Q_PROPERTY(type m_total READ getTotal WRITE setTotal)
+    Q_PROPERTY(QString m_name READ getName WRITE setName)
+    Q_PROPERTY(QDate m_date READ getDate WRITE setDate)
+    Q_PROPERTY(double m_total READ getTotal WRITE setTotal)
 
 public:
-    explicit ShoppingCart(QObject *parent = nullptr);
-    ShoppingCart(QString name, double total): m_name(name), m_total(total) {
-        date = currentDate();
-    };
+    explicit ShoppingCart(QString name, double total, QObject *parent = nullptr) : m_name(name), m_total(total),QObject(parent) {
+        m_date = QDate::currentDate();
+    }
     QString getName() const;
     QDate getDate() const;
     double getTotal() const;
-    void setName();
-    void setDate();
-    void setTotal();
+    void setName(QString name);
+    void setDate(QDate date);
+    void setTotal(double total);
 
 private:
     QString m_name;
     QDate m_date;
     double m_total;
-
-signals:
-
 };
 
 
